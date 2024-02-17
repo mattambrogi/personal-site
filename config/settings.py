0fc9ea71c65efd7743d11b4add4ced90ed91790b
Matt <matthew.ambrogi@gmail.com>
2024-02-17 11:59:01 -0500

    Updated CKEditor configs for better text editing options

--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,39 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'Custom',
+        'toolbar_Custom': [
+            ['Bold', 'Italic', 'Underline', 'Strike'],
+            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent'],
+            ['Link', 'Unlink'],
+            ['RemoveFormat', 'Source'],
+            ['CodeSnippet'],
+            ['HorizontalRule'],
+            ['TextColor', 'BGColor'],
+            ['Smiley'],
+            ['SpecialChar'],
+            ['Image'],
+            ['Table'],
+        ],
+        'extraPlugins': ','.join([
+            'liststyle',
+            'format',
+            'codesnippet',
+            'specialchar',
+            'image2',
+            'table',
+            'colorbutton',
+            'horizontalrule',
+        ]),
+        'removePlugins': 'image',
+        'image2_toolbar': [
+            'ImageFull',
+            'ImageSide',
+            'ImageTextAlternative',
+        ],
+    }
+}
